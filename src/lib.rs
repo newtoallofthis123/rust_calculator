badc318962d30fd1b3940ee01aa67648ac61b90e
NoobScience <noobscience123@gmail.com>
2025-02-24 19:43:47 +0530

    feat: Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,27 @@ pub fn evaluate(input: &str) -> Result<i64, EvalexprError> {
     eval_int(input)
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_add() {
+        assert_eq!(evaluate("2+2"), "4");
+    }
+
+    #[test]
+    fn test_bodmas() {
+        assert_eq!(evaluate("5+2*3"), "11");
+    }
+
+    #[test]
+    fn test_all_operators() {
+        assert_eq!(evaluate("10+2-3*4/2"), "6");
+    }
+
+    #[test]
+    fn simple_division() {
+        assert_eq!(evaluate("2/1"), "2");
+    }
+}
